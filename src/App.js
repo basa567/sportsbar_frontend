78370a03b5301eff96ce3ce4ab0052afe0783540
Samita <kcsamita971@gmail.com>
2017-04-04 12:23:01 +0300

    Revert "added static map with a marker"
    This reverts commit be755005f115ee8ec7035dbba2478d3d91de01c6.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Search from './components/Search.js';
-import Map from './components/Map.js';
+import Navigation from './components/Navigation.js';
 import Bar from './components/Bar.js';
 import Sport from './components/Sport.js';
 import Contact from './components/Contact.js';
@@ -8,32 +8,16 @@ import "./assests/style.css";
 
 class App extends Component {
   render() {
-    const location = {
-      lat: 65.0123600,
-      lng:  25.4681600
-    }
-
-    const markers = [
-      {
-        location: {
-          lat: 65.0123600,
-          lng: 25.4681600
-        }
-      }
-    ]
-
     return (
       <div>
         <Search/>
-        <div style={{width:'100 %', height:500}}>
-          <Map center={location} markers={markers}/>
-        </div>
+        <Navigation/>
         <Bar/>
-        <Sport/>
-        <Contact/>
+        <Sport/>   
+        <Contact/>     
       </div>
     );
   }
 }
 
- export default App;
+export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Navigation extends Component {
+  render() {
+    return (
+      <div className="cover nav">
+        <h2>Add UI design for navigation</h2>
+      </div>
+    );
+  }
+}
+
+export default Navigation;
