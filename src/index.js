d5898fafa28055452fc87d69d308891b30586d65
kcsamita <kcsamita971@gmail.com>
2017-04-06 21:09:41 +0300

    remove comment

--- src/index.js
+++ src/index.js
@@ -12,45 +12,32 @@ import { Router, Route, browserHistory,Link } from 'react-router'
 
 class  App extends Component {
   render() {
-    return (
-    //   <div classNameNameNameName="container">      
-    //       <div classNameNameNameName="row">
-    //          <ul>
-    //                <li><Link to="/search">Search</Link></li>
-    //                <li><Link to="/top3bar">Top 3 Bar</Link></li>                        
-    //          </ul>
-    //       </div>           
-    //     {this.props.children}           
-    //     <Map/>        
-    //     <Sport/>
-    //     <Bar/>
-    //     <Contact/>        
-    //   </div>
-    // <div>   
-  
-<nav className="navbar navbar-default">
-  <div className="container-fluid">  
-    <div className="navbar-header logo">
-      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span className="sr-only">Toggle navigation</span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-      </button>
-      <a className="navbar-brand title " href="#">Sports Bar</a>
-    </div>   
-    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul className="nav navbar-nav">
-         <li><Link to="/">Home</Link></li>
-         <li><Link to="/search">Search</Link></li>
-         <li><Link to="/map">Map</Link></li>
-         <li><Link to="/top3bar">Top 3 Bar</Link></li> 
-         <li><Link to="/contact">Contact</Link></li> 
-      </ul>     
-    </div>
-  </div>
-  {this.props.children}  
-</nav>
+    return ( 
+     <div>
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">  
+          <div className="navbar-header logo">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand title " href="#">Sports Bar</a>
+          </div>   
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul className="nav navbar-nav">
+               <li><Link to="/">Home</Link></li>
+               <li><Link to="/search">Search</Link></li>
+               <li><Link to="/map">Map</Link></li>
+               <li><Link to="/top3bar">Top 3 Bar</Link></li> 
+               <li><Link to="/contact">Contact</Link></li> 
+            </ul>     
+          </div>
+        </div>
+        {this.props.children}  
+      </nav>
+     </div>
     );
   }
 }
