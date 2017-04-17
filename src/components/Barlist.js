facf3dd3c51787607200b86e282dee520bdc6934
melen <t5tame01@students.oamk.fi>
2017-04-17 14:45:45 +0300

    barlist

--- src/components/Barlist.js
+++ src/components/Barlist.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import {barimg} from './image/barimg.png';
+
 
 class Barlist extends Component {
   render() {
     return (
       <li className="barlist">
-    <img src={require("./image/barimg.png")} alt="sportsbar" width="350px" height="150px"  className="bar"></img>
-
+   <img src={require("./image/" + this.props.topbar.image)} alt="sportsbar" width="350px" height="150px"  className="bar"></img>
+        {/* <img src={require(this.props.topbar.image)/></img> */}
         <h6>{this.props.topbar.barname}</h6>
         <h6>{this.props.topbar.address}</h6>
         <h6>{this.props.topbar.rating}</h6>
