948a3979bb53e5398d4d62e8c4af798a2095b311
sumankharel <javelin.suman@gmail.com>
2017-04-18 13:52:44 +0300

    fixed mixed contents error in review

--- src/components/Review.js
+++ src/components/Review.js
@@ -5,62 +5,62 @@ class Review extends Component {
    constructor(props) {
           super(props);
           this.state = {
-            rating:[]            
-          }                          
+            rating:[]
+          }
       }
-  
+
    componentDidMount() {
-       var url ="http://sportsbbar.herokuapp.com/getbarrate/"+this.props.params.bid;           
-       this.api={          
-              getreview(){               
-                  return fetch(url).then((res)=>res.json());                   
+       var url ="https://sportsbbar.herokuapp.com/getbarrate/"+this.props.params.bid;           
+       this.api={
+              getreview(){
+                  return fetch(url).then((res)=>res.json());
               }
-        };       
-       this.api.getreview().then((res)=>{         
+        };
+       this.api.getreview().then((res)=>{
            this.setState({
               rating:res
-            });                                       
-        })   
-                           
-   }  
-  render() {    
-    const ratinglength = this.state.rating.length; 
-    console.log(ratinglength);   
+            });
+        })
+
+   }
+  render() {
+    const ratinglength = this.state.rating.length;
+    console.log(ratinglength);
     if(ratinglength>0){
       var review = this.state.rating.map((time, i) => <ReviewResult key={i} data={time} />)
     }
     return (
     <div>
       <div className="container">
-          <div className="row">                    
-              <div className="col col-sm-12 ">            
-                <div className="row">                                  
-                    <div className="col col-lg-12 ">                 
+          <div className="row">
+              <div className="col col-sm-12 ">
+                <div className="row">
+                    <div className="col col-lg-12 ">
                     <Link to={"detail/"+this.props.params.bid }> <button type="button" className="btn btn-primary gobackbtn">Go Back</button></Link>
                     </div>
-                </div>             
-              </div>                             
-          </div>            
-      </div>  
-      {review}     
-     </div> 
+                </div>
+              </div>
+          </div>
+      </div>
+      {review}
+     </div>
     );
   }
 }
 
- class ReviewResult extends Component { 
-      render() {                                              
+ class ReviewResult extends Component {
+      render() {
             return (
-                <div className="container review">       
-                  <div className="row">  
-                      <div className="col col-sm-12 titlename">            
-                        <h4>{this.props.data.name} says</h4>               
-                      </div>                    
-                      <div className="col col-sm-12 ">            
-                        <p class="cmt">{this.props.data.review}</p>               
-                      </div>                             
-                  </div> 
-               </div>                    
+                <div className="container review">
+                  <div className="row">
+                      <div className="col col-sm-12 titlename">
+                        <h4>{this.props.data.name} says</h4>
+                      </div>
+                      <div className="col col-sm-12 ">
+                        <p class="cmt">{this.props.data.review}</p>
+                      </div>
+                  </div>
+               </div>
             )
          }
        }
