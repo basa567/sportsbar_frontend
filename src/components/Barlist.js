16e8d188a9b0f12c82a7fa7edfa29058fbced367
sumankharel <javelin.suman@gmail.com>
2017-04-20 15:03:57 +0300

    optimized bar list for bigger screens

--- src/components/Barlist.js
+++ src/components/Barlist.js
@@ -7,12 +7,17 @@ class Barlist extends Component {
     return (
       <Link to={"detail/"+this.props.topbar.bar_id }>
       <div className="barlist">
+      <div className="row">
+      <div className="col-sm-12 col-md-6">
+        <img src={require("./image/" + this.props.topbar.image)} alt="sportsbar" width="350px" height="150px"  className="bar"></img>
+      </div>
 
-       <img src={require("./image/" + this.props.topbar.image)} alt="sportsbar" width="350px" height="150px"  className="bar"></img>
-
+      <div className="col-sm-12 col-md-6">
         <h4>{this.props.topbar.barname}</h4>
         <h6>{this.props.topbar.address}</h6>
         <h6>Rating: {this.props.topbar.rating}</h6>
+      </div>
+      </div>
       </div>
       </Link>
     );
